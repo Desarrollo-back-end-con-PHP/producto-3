250e148dd8079ef52d85d0369645ded26da314e7
Thatatadm <mthabatadenise@gmail.com>
2025-11-22 13:02:07 +0100

    Corregido el use en AuthController para importar correctamente Authenticatable

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\TransferViajero;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class AuthController extends Controller
 {
